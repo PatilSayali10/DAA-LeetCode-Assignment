eb9b0b2202e64e12e3f83b9a592f57b1ab2c1e4b
PatilSayali10 <sp8389880@gmail.com>
2026-08-06 20:12:30 +0530

    Update Question3.java

--- Answers/Question3.java
+++ Answers/Question3.java
@@ -1,1 +1,39 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
+        int start = 0, end = m - 1;
+        int row = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target >= matrix[mid][0]) {
+                row = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        start = 0;
+        end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+
+            if (matrix[row][mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
