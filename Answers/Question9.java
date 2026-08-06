2bfed9e8db2938e709301b2591e438a0a53f6b6f
PatilSayali10 <sp8389880@gmail.com>
2026-08-06 12:11:48 +0530

    Update Question9.java

--- Answers/Question9.java
+++ Answers/Question9.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        
+
+        int sum = 0;
+        int max = nums[0];
+
         for (int i = 0; i < nums.length; i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+
+            sum = sum + nums[i];
+
+            if (sum > max)
+                max = sum;
+
+            if (sum < 0)
+                sum = 0;
         }
-        
-        return maxSum;
+
+        return max;
     }
 }
